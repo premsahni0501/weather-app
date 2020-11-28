a85af10ab15c00da5c530510bbc4015e4efb3682
premsahni0501 <premsahni0501@gmail.com>
2020-11-28 19:41:24 +0530

    Updates

---
+++ js/Details.c3732e17.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["Details"],{"9f52":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],i=n("2877"),l={},u=Object(i["a"])(l,a,s,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=Details.c3732e17.js.map
